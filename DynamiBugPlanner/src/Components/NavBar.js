26d64259745178c5a355fadd6b633499219b10b1
AtVict0r <oluwasegun.atolagbe@mail.citytech.cuny.edu>
2023-02-08 18:21:38 -0500

    Tried to add user login option

--- DynamiBugPlanner/src/Components/NavBar.js
+++ DynamiBugPlanner/src/Components/NavBar.js
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import * as React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
@@ -18,6 +21,26 @@ import Stack from 'react-bootstrap/Stack';
 //     )
 // }
 
+const User = {
+    'IsSignedIn': true,
+    'Name': "John Doe",
+}
+
+function BrandLogo() {
+    return (
+        <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="./logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            DynamiBug Planner
+        </Navbar.Brand>
+    );
+}
+
 function SearchComponent() {
     return (
       <Stack direction="horizontal" gap={3}>
@@ -27,9 +50,46 @@ function SearchComponent() {
     );
 }
 
+function UserLogin() {
+    let content;
+
+    if(User.IsSignedIn){ 
+        content = (
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#action1">Login</Nav.Link>
+                <Nav.Link href="#action2">Register</Nav.Link>
+            </Nav>
+        );
+    } else {
+        content = (
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#action1">{User.Name}</Nav.Link>
+                <Nav.Link href="#action2">Logout</Nav.Link>
+            </Nav>
+        );
+    }
+
+    return(
+        {content}
+        <Nav
+        // To=Do: Add Profile Image
+    );
+}
+
 function NavBar() {
     return (
-        <SearchComponent />
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <BrandLogo />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <SearchComponent />
+                        <UserLogin />
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
   
